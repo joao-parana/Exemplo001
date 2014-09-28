3ae1a8eabbc6282a605b1700d8b71c04172176df
Joao Ferreira <joao.parana@gmail.com>
2014-09-27 22:14:18 -0300

    Alterando o Label programaticamente

--- Exemplo001/ViewController.swift
+++ Exemplo001/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  @IBOutlet weak var myLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    println("Oi Mundo")
+    myLabel.text = "Funcionou"
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
 
