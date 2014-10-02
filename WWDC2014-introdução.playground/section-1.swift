06a2257125f4db20eab06e26a8f4ba7bfd37c555
João Antonio Ferreira <joao.parana@gmail.com>
2014-10-02 16:26:00 -0300

    Alterações no  playground adicionando parte do TODO

--- WWDC2014-introdução.playground/section-1.swift
+++ WWDC2014-introdução.playground/section-1.swift
@@ -4,41 +4,78 @@ import UIKit
 
 var str = "Hello, playground"
 
+// LOOP para teste
 for i in 0...100 {
   i * i
 }
 
 //let color = "blue"
-// let attrStr = NSAttributedString(string: str, attributes: [NSForegroundColorAttributeName : color]?)
+// let attrStr = NSAttributedString(string: str, 
+//                   attributes: [NSForegroundColorAttributeName : color]?)
 
-class DataSource: NSObject,UITableViewDataSource {
+// Criando uma fonte de dado para uma TableView () 
+
+class DataSource: NSObject, UITableViewDataSource {
+  
+  var array = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5",
+               "Item 6", "Item 7", "Item 8", "Item 9"]
   
   func tableView(tableView: UITableView,
                   numberOfRowsInSection section: Int) -> Int  {
-    return 4
+    return array.count
   }
   
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+  func tableView(tableView: UITableView,
+          cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     
     let row = indexPath.row
-    let myStyle = UITableViewCellStyle.fromRaw(row)
+    let myStyle = UITableViewCellStyle.fromRaw(row % 4)
     
     var tableViewCellStyle : UITableViewCellStyle = .Default
     if (myStyle != nil) {
       tableViewCellStyle = myStyle!
     }
     
-    let cell = UITableViewCell(style: tableViewCellStyle, reuseIdentifier: nil)
+    let cell = UITableViewCell(style: tableViewCellStyle, reuseIdentifier: "Cell")
     
-    cell.textLabel?.text = "Text"
+    cell.textLabel?.text = array[indexPath.row]
     
     return cell
   }
 }
 
 let ds = DataSource()
-let tableView = UITableView(frame: CGRect(x:0,y:0,width:320,height:240), style: .Plain)
+// criando a TableView
+var myFrame = CGRect(x:0, y:0, width:320, height:460)
+let tableView = UITableView(frame: myFrame, style: .Plain)
 
+// Associando a Fonte de Tado com a View para exibir na tela
 tableView.dataSource = ds
 
 tableView.reloadData()
+
+// -----
+
+// Persistência
+let persistence = NSUserDefaults.standardUserDefaults()
+
+// Verificando se existe o elemento
+
+let existe: AnyObject? = persistence.objectForKey("myName")
+if (existe == nil) {
+  // Não existe o elemento então devo criar
+  persistence.setObject("João", forKey: "myName")
+  persistence.synchronize()
+}
+
+persistence.objectForKey("myName")
+
+persistence.setObject("Jorge", forKey: "myName")
+persistence.synchronize()
+
+persistence.objectForKey("myName")
+
+
+
+
+
